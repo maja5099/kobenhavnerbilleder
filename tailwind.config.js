4ddde43d845d20c81b30bed09fe0c15b1decb6f8
Maja Sørensen <majabirchsorensen@gmail.com>
2022-02-03 15:14:20 +0100

    font and color added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,16 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      background: "#CDCDCD",
+      h2: "#114E60",
+      h3: "#325288",
+    },
+    fontFamily: {
+      caveat: "'Caveat', system-ui, sans-serif",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
